14a73fca205fedbc6b05eee4962245b761b15b1a
Daniel Barría Andrade <danielbarriaandrade@Daniels-MacBook-Air.local>
2020-11-24 21:33:11 -0300

    crear orden

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React,{useState} from "react";
 import { useCartContext } from "../../context/cartContext";
 import { Link } from "react-router-dom";
+import { getFirestore} from "../../firebase";
+import * as firebase from "firebase/app";
+
 
 const Cart = () => {
   const { cart, remove, total } = useCartContext();
+  const [checkout, setCheckout ] = useState('');
+
+
+  async function createOrder() {
+    const order = {
+      buyer: { name: 'Daniel', phone: '123123', email: 'asd@asd'},
+      items: cart.map((item) => (
+        {id: item.id, title: item.titulo, price: item.price, quantity: item.total}
+      )),
+      date: firebase.firestore.FieldValue.serverTimestamp(),
+      total: total(),
+    }
+
+    const db = getFirestore();
+    const orders = db.collection("orders");
+
+    
+    
+    // cart.forEach(c => {
+    //   let item = db.collection("items").where(firebase.firestore.FieldPath.documentId(), 'in', c.id).get();
+    //   item.ref.update({ stock: item.data().stock - c.total })
+    // });
+
+    try {
+      const id = await orders.add(order);
+      console.log('order creada id: ', id);
+      setCheckout(id.id);
+      
+    } catch (err) {
+      console.log('error', err);
+      console.log(order);
+    }
+    
+
+  }
 
   return (
     <div className="container" style={{ paddingTop: 30 }}>
@@ -59,7 +97,32 @@ const Cart = () => {
               ))}
               {total() !== 0 ? (
                 <div className="nes-container is-rounded col-md-12">
-                  <div className="row">total: ${total()}</div>
+                  <div className="row" style={{ paddingRight: 22 }}>
+                    <div className="col-md-9">
+                    
+                    {checkout == '' ? ( <div>total: ${total()}</div>) : (<div>tu id de orden es: {checkout}</div>)}
+                    </div>
+                    <div className="col-md-3">
+                    <button
+                        type="button"
+                        className="nes-btn is-warning"
+                        onClick={() => createOrder()}
+                        style={{ width: 300 }}
+                      >
+                        crear orden
+                      </button>
+                    {/* <Link to="/">
+                      <button
+                        type="button"
+                        className="nes-btn is-warning"
+                        style={{ width: 300 }}
+                      >
+                        checkout
+                      </button>
+                    </Link> */}
+                    </div>
+                    </div>
+
                 </div>
               ) : (
                 <div className="nes-container is-rounded col-md-12">
